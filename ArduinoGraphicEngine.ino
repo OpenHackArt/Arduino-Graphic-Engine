08725d4fa27cc16dc13141c142c88105be1bcf20
Yuqing_Chen <chenyuqing0429@foxmail.com>
2021-06-08 18:36:41 +0800

    ILI9481调试成功

--- ArduinoGraphicEngine.ino
+++ ArduinoGraphicEngine.ino
@@ -1,31 +1,22 @@
-#include "SPI.h"
-#include <Adafruit_GFX.h>
-#include <ILI9488.h>
+#include <TFT_HX8357_Due.h>
 #include "color.h"
 #include "geometry.h"
 #include "math.h"
 
-#define TFT_CS 7
-#define TFT_DC 9
-#define TFT_LED 8
-#define TFT_RST 10
+#define K1 10
+#define K2 11
+#define K3 12
+#define K4 13
 
 #define FOV 60
-#define TICK_PER_FRAME 17 // 60 fps
-#define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 480
-#define HALF_SCREEN_WIDTH 160
-#define HALF_SCREEN_HEIGHT 240
+#define SCREEN_WIDTH 480
+#define SCREEN_HEIGHT 320
+#define HALF_SCREEN_WIDTH 240
+#define HALF_SCREEN_HEIGHT 160
 
-#define MAX_VERTEX 200
-
-ILI9488 tft = ILI9488(TFT_CS, TFT_DC, TFT_RST);
-
-float ticks = 0;
-unsigned int loops = 0;
+TFT_HX8357_Due tft = TFT_HX8357_Due();
 
 Matrix4f m_world;
-unsigned short render_mode = 1; // 0 - orthogonal | 1 - perspective
 
 void mode_ui(const uint16_t color)
 {
@@ -33,16 +24,6 @@ void mode_ui(const uint16_t color)
   tft.setTextColor(color);
   tft.setTextSize(2);
   tft.println("Author: @cyq");
-  if (render_mode == 0)
-  {
-    tft.println("Mode: Orthogonal");
-  }
-  else
-  {
-    tft.println("Mode: Perspective");
-  }
-  tft.print("Max vertex: ");
-  tft.println(MAX_VERTEX);
 }
 
 void draw_vertex(int vertex_cnts, Vector2f *projected, const uint16_t color)
@@ -53,14 +34,15 @@ void draw_vertex(int vertex_cnts, Vector2f *projected, const uint16_t color)
   }
 }
 
-void draw_triangles(int vertex_cnts, Vector2f *projected, const Vector3i *tris, const uint16_t color)
+void draw_triangles(int triangle_cnts, Vector2f *projected, const Vector3i *indices, const uint16_t color)
 {
-  for (int i = 0; i < vertex_cnts; i++)
+  for (int i = 0; i < triangle_cnts; i++)
   {
-    // draw triangle edges - 0 -> 1 -> 2 -> 0
-    tft.drawLine(projected[tris[i].x].x, projected[tris[i].x].y, projected[tris[i].y].x, projected[tris[i].y].y, color);
-    tft.drawLine(projected[tris[i].y].x, projected[tris[i].y].y, projected[tris[i].z].x, projected[tris[i].z].y, color);
-    tft.drawLine(projected[tris[i].z].x, projected[tris[i].z].y, projected[tris[i].x].x, projected[tris[i].x].y, color);
+    Vector3i index = indices[i];
+    Vector2f v0 = projected[index.x];
+    Vector2f v1 = projected[index.y];
+    Vector2f v2 = projected[index.z];
+    tft.drawTriangle(v0.x, v0.y, v1.x, v1.y, v2.x, v2.y, color);
   }
 }
 
@@ -68,51 +50,37 @@ void setup()
 {
   Serial.begin(9600);
   tft.begin();
-  tft.setRotation(0);
+  tft.setRotation(1);
 
-  // read diagnostics (optional but can help debug problems)
-  uint8_t x = tft.readcommand8(ILI9488_RDMODE);
-  Serial.print("Display Power Mode: 0x");
-  Serial.println(x, HEX);
-  x = tft.readcommand8(ILI9488_RDMADCTL);
-  Serial.print("MADCTL Mode: 0x");
-  Serial.println(x, HEX);
-  x = tft.readcommand8(ILI9488_RDPIXFMT);
-  Serial.print("Pixel Format: 0x");
-  Serial.println(x, HEX);
-  x = tft.readcommand8(ILI9488_RDIMGFMT);
-  Serial.print("Image Format: 0x");
-  Serial.println(x, HEX);
-  x = tft.readcommand8(ILI9488_RDSELFDIAG);
-  Serial.print("Self Diagnostic: 0x");
-  Serial.println(x, HEX);
-
-  tft.fillScreen(WHITE);
-  mode_ui(BLACK);
-  ticks = millis();
-  m_world = mMultiply(mScale(30), m_world);
+  tft.fillScreen(BLACK);
+  mode_ui(WHITE);
+  m_world = mMultiply(mScale(0.3), m_world);
 }
 
 void loop(void)
 {
-  cube obj;
-  while (millis() > ticks)
+  int start = millis();
+  sephere obj;
+
+  m_world = mMultiply(mRotateY(5), m_world);
+  m_world = mMultiply(mRotateX(5), m_world);
+  m_world = mMultiply(mRotateZ(5), m_world);
+
+  // for future, z-buffer
+  for (int i = 0; i < obj.vertex_counts; i++)
   {
-    // m_world = mMultiply(mRotateX(10), m_world);
-
-    // for future, z-buffer
-    for (int i = 0; i < obj.vertex_counts; i++)
-    {
-      // no camera
-      // project to screen space directly
-      Vector3f v = m_mul_v(m_world, obj.vertex_pos[i]);
-      obj.projected_vertex[i].x = (FOV * v.x) / (FOV + v.z) + HALF_SCREEN_WIDTH;
-      obj.projected_vertex[i].y = (FOV * v.y) / (FOV + v.z) + HALF_SCREEN_HEIGHT;
-    }
-
-    ticks += TICK_PER_FRAME;
+    // no camera
+    // project to screen space directly
+    Vector3f v = m_mul_v(m_world, obj.vertex_pos[i]);
+    obj.projected_vertex[i].x = (FOV * v.x) / (FOV + v.z) + HALF_SCREEN_WIDTH;
+    obj.projected_vertex[i].y = (FOV * v.y) / (FOV + v.z) + HALF_SCREEN_HEIGHT;
   }
 
-  draw_vertex(obj.vertex_counts, obj.projected_vertex, BLACK);
-  draw_triangles(obj.vertex_counts, obj.projected_vertex, obj.triangles, RED);
+  tft.fillScreen(BLACK);
+  draw_triangles(obj.triangle_counts, obj.projected_vertex, obj.indices, GREEN);
+  // clear frame
+  // draw_triangles(obj.triangle_counts, obj.projected_vertex, obj.indices, WHITE);
+
+  int end = millis();
+  Serial.println(1000 / (end - start));
 }
